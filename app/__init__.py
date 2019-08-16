be877717b59d3939a8c29a5f55c43ffd972c4809
jack <137429139@qq.com>
2019-08-16 21:26:39 +0800

    项目结构初始化

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+"""
+===========================
+File Name: __init__.py.py
+Date: 2019/8/16
+Software: PyCharm
+Author: li_pin
+===========================
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
